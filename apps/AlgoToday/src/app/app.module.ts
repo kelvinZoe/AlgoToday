734a89693f5b99b93ff419c92aca07ca3f5d16bb
Kelvin Ewusie Arthur <61987203+kelvinZoe@users.noreply.github.com>
2024-02-07 16:00:17 +0000

    Add code for angular-17.0.0 integrate-side-nav.
    [Url: razroo.com/community/angular-17.0.0/side-nav-recipe/integrate-side-nav]

--- apps/AlgoToday/src/app/app.module.ts
+++ apps/AlgoToday/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./routing/app-routing.module";
 import { GlobalHeaderComponentModule } from "@AlgoToday/common/ui";
 import { GlobalFooterModule } from "@AlgoToday/common/ui";
+import { GlobalSidenavModule } from "@AlgoToday/common/ui";
+import { MatListModule } from "@angular/material/list";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, AlgoToday, GlobalFooterModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, AlgoToday, GlobalFooterModule, GlobalSidenavModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
